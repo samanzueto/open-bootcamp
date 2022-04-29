91976f92bba5ad1dc041a1ad4575a691dc9e7794
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-04-29 18:51:41 -0500

    Errores de escritura corregidos

--- introduccion-programacion/ejercicio-tema4/main.java
+++ introduccion-programacion/ejercicio-tema4/main.java
@@ -2,30 +2,30 @@ package org.example;
 
 /*************************************
 
-- Ejercicio Tema 4 
+ - Ejercicio Tema 4
 
-Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
-Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+ Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+ Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
 
-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+ Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
 
-Incrementar el valor de la variable en uno cada vez que se ejecute.
+ Incrementar el valor de la variable en uno cada vez que se ejecute.
 
-Mostrarlo por pantalla cada vez que se ejecute.
+ Mostrarlo por pantalla cada vez que se ejecute.
 
-Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+ Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
 
-Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+ Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
 
-Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+ Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
 
-*************************************/
+ *************************************/
 
 public class Main {
     public static void main(String[] args) {
 
         // Imprimir mensaje de la funcion numero
-        System.out.println(numero(20));
+        System.out.println(numero(-20));
 
         // Declaramos variable numeroWhile
         int numeroWhile = 1;
@@ -36,7 +36,7 @@ public class Main {
             numeroWhile++;
 
             // Imprimir mensaje
-            System.out.println(numeroWhile);
+            System.out.println("While: " + numeroWhile);
         }
 
         // Condicional numeroWhile sea menor que 3
@@ -45,18 +45,18 @@ public class Main {
             numeroWhile++;
 
             // Imprimir mensaje
-            System.out.println(numeroWhile);
-        }while( numeroWhile < 3 )
+            System.out.println("Do While: " +numeroWhile);
+        }while( numeroWhile < 3 );
 
         for(int numeroFor=0; numeroFor<=3; numeroFor++){
-            System.out.println(numeroFor);
+            System.out.println("For: " +numeroFor);
         }
 
-        String estacion = "invierno";
+        String estacion = "verano";
         switch(estacion){
             case "primareva":
             case "verano":
-            case "otoño"
+            case "otoño":
             case "invierno":
                 System.out.println("Es "+estacion);
                 break;
@@ -70,9 +70,9 @@ public class Main {
         String msg = "Es un 0 (cero)";
 
         // Condicional menor que 0 => negativo
-        if(numeroIf < 0) msg = "Es un número negativo";
-        // Condicional mayor que 0 => positivo
-        else if(numeroIf > 0) msg = "Es número positivo"
+        if(numeroIf < 0) msg = "Es negativo";
+            // Condicional mayor que 0 => positivo
+        else if(numeroIf > 0) msg = "Es positivo";
 
         // Devuelve mensaje
         return msg;
