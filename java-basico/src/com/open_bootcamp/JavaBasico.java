c91be55f957f76c9b8a05fdee36e7e4c4bdb598f
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-04-30 00:15:28 -0500

    Iniciando el curso de Java Basico

---
+++ java-basico/src/com/open_bootcamp/JavaBasico.java
@@ -0,0 +1,9 @@
+package com.open_bootcamp;
+
+public class JavaBasico {
+
+    public static void main(String[] args){
+        // Hacer lo que sea
+        System.out.println("Bienvenido");
+    }
+}
