014b94b6cd11c88c5705dcdd22fb74c505a9e03f
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-04-30 00:35:40 -0500

    Limpiando carpeta java-basico

--- java-basico/src/com/open_bootcamp/JavaBasico.java
+++
@@ -1,9 +0,0 @@
-package com.open_bootcamp;
-
-public class JavaBasico {
-
-    public static void main(String[] args){
-        // Hacer lo que sea
-        System.out.println("Bienvenido");
-    }
-}
