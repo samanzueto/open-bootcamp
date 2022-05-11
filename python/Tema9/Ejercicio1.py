68ea771d5ce1359448ade9bd10f133dea56d7b0f
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-05-11 02:04:11 -0500

    Actualizo Ejercicios 1 y 2 del Tema 9 - Phyton

--- python/Tema9/Ejercicio1.py
+++ python/Tema9/Ejercicio1.py
@@ -2,5 +2,7 @@ paises = list(str(input('\nLista de paises (separados por coma): ')).split(','))
 
 ordenados = set(sorted(map(str.strip,paises)))
 
-for pais in ordenados:
-    print(f'{pais.title()},')
+#for pais in ordenados:
+#    print(f'{pais.title()},')
+
+print(", ".join(map(str.title, ordenados)))
--- python/Tema9/Ejercicio2.py
+++ python/Tema9/Ejercicio2.py
@@ -1,10 +1,9 @@
 from functools import reduce
 
-# Hay un error en la linea 3 al momento de recibir parametros
+def sumatoria_impares(lista):
+    odds = filter(lambda x: x % 2, lista)
+    res = reduce(lambda a, b: a + b, odds)
+    print(f'La sumatoria es: {res}')
+
 nums = list(map(int,input("\nIntroduce los números (separados por espacio): ").strip().split()))
-
-odds = filter(lambda x: x % 2, nums)
-
-res = reduce(lambda a, b: a + b, odds)
-
-print(f'La sumatoria es: {res}')
+sumatoria_impares(nums)
