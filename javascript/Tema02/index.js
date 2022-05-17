7d2324c1eed5d0966eb52c21bf3b80cc87da2bf3
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-05-17 18:19:37 -0500

    Subo Ejercicio 01 Tema 02 - JavaScript

---
+++ javascript/Tema02/index.js
@@ -0,0 +1,12 @@
+let nombre = "Sam Anzueto";
+let edad = 30;
+let isDesarrollador = true;
+let fechaNacimiento = new Date(1991,9,9);
+let libroFavorito = {
+    titulo: "El principito",
+    autor: "Antoine de Saint-Exupéry",
+    fecha: new Date(1943,3,6),
+    url: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiep8HL1Of3AhUyK0QIHSgBA5cQFnoECAYQAQ&url=http%3A%2F%2Fbibliotecadigital.ilce.edu.mx%2FColecciones%2FObrasClasicas%2F_docs%2FElPrincipito.pdf&usg=AOvVaw0x78lN8rUsrkaq7LtShPQw"
+}
+
+console.log(nombre, edad, isDesarrollador, fechaNacimiento, libroFavorito)
