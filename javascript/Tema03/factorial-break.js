88a872a29e9e210a70ba47f43283806537fde99c
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-05-19 18:13:18 -0500

    Subo Ejercicio 01 del Tema 03 - JavaScript

---
+++ javascript/Tema03/factorial-break.js
@@ -0,0 +1,10 @@
+let n = 5;
+let f = 1;
+while(true){
+    if(n==0){
+        break;
+    }
+    f*=n;
+    n--;
+}
+console.log(f)
---
+++ javascript/Tema03/factorial-while.js
@@ -0,0 +1,7 @@
+let n = 5;
+let f = 1;
+while(n > 0){
+    f*=n;
+    n--;
+}
+console.log(f)
