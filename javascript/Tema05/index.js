ae53c880de1e9ee4666142fca4c61b5b8530f6e2
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-05-19 22:51:10 -0500

    Actualizo Ejercicio 01 del Tema 05 - JavaScript

--- javascript/Tema05/index.js
+++ javascript/Tema05/index.js
@@ -3,5 +3,5 @@ let alturaM = 1.53
 let peso = 60.5
 let alturaRedondeada = Math.ceil(alturaM);
 let pesoRedondeado = Math.floor(peso);
-let max = Number.MAX_VALUE + 1;
-console.log(alturaCM, alturaM, peso, alturaRedondeada, pesoRedondeado, max);
+let esIgual = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(alturaCM, alturaM, peso, alturaRedondeada, pesoRedondeado, esIgual);
