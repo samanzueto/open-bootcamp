dab6078c6ea8dac471a08931b4abfec1951e3406
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-05-19 22:47:33 -0500

    Subo Ejercicio 01 del Tema 05 - JavaScript

---
+++ javascript/Tema05/index.js
@@ -0,0 +1,7 @@
+let alturaCM = 153;
+let alturaM = 1.53
+let peso = 60.5
+let alturaRedondeada = Math.ceil(alturaM);
+let pesoRedondeado = Math.floor(peso);
+let max = Number.MAX_VALUE + 1;
+console.log(alturaCM, alturaM, peso, alturaRedondeada, pesoRedondeado, max);
