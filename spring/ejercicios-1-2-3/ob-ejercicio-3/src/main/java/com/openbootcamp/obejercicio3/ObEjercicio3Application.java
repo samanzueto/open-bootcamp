ce3833cd59b1dadae72b433e602bc46ffa118478
Sam Anzueto <samanzueto@users.noreply.github.com>
2022-07-19 00:04:55 -0500

    Subo Ejercicio 03 - Spring

---
+++ spring/ejercicios-1-2-3/ob-ejercicio-3/src/main/java/com/openbootcamp/obejercicio3/ObEjercicio3Application.java
@@ -0,0 +1,13 @@
+package com.openbootcamp.obejercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObEjercicio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObEjercicio3Application.class, args);
+	}
+
+}
---
+++ spring/ejercicios-1-2-3/ob-ejercicio-3/src/test/java/com/openbootcamp/obejercicio3/ObEjercicio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbootcamp.obejercicio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObEjercicio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
